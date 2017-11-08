43c961975b88862fe6f4bd814ed3167b8d3fa353
Kevin Lin <developer@kevinlin.info>
2017-11-08 13:37:45 -0600

    Proxy mission request to correct service based on registered port
    Summary:
    Motivation: support multiple, concurrent missions while abstracting out the actual mechanism of connection.
    
    Change: When a mission proxy request is received, look up the self-registered service port by ID, and proxy to that route.
    
    Note that this is a breaking API change. The only client that depends on this API is Skycommand, which will need to be updated to conform to the new spec. Tracking in T114.
    
    Test Plan: Manual end-to-end test.
    
    Reviewers: #skynet, emilio
    
    Reviewed By: #skynet, emilio
    
    Maniphest Tasks: T99
    
    Differential Revision: https://code.skynet.engineering/D60

--- src/handlers/mission_proxy_handler.py
+++ src/handlers/mission_proxy_handler.py
@@ -1,11 +1,9 @@
-import os
-
 import flask
 import requests
+from redis import StrictRedis
 from requests import ConnectionError
 
 from handlers.base_handler import BaseHandler
-
 
 REQUEST_METHOD_FUNC = {
     'GET': requests.get,
@@ -18,17 +16,31 @@ class MissionProxyHandler(BaseHandler):
     Proxy handler to Skymission for all mission action requests.
     """
 
-    path = '/mission/<path:mission_url>'
+    path = '/mission/<mission_id>/<path:mission_url>'
     async = False
 
-    def run(self, input_data, mission_url, *args, **kwargs):
+    def __init__(self):
+        super(MissionProxyHandler, self).__init__()
+        self.redis = StrictRedis()
+
+    def run(self, input_data, mission_id, mission_url, *args, **kwargs):
         request_func = REQUEST_METHOD_FUNC[flask.request.method]
-        skymission_port = os.getenv('SKYMISSION_PORT', 4000)
+        key = 'skymission:mission:{mission_id}'.format(mission_id=mission_id)
+        skymission_port = self.redis.get(key)
+
+        if not skymission_port:
+            return {
+                'message': 'Service port for `{mission_id}` not registered!'.format(
+                    mission_id=mission_id,
+                )
+            }
 
         url = 'http://127.0.0.1:{skymission_port}/{path}'.format(
             skymission_port=skymission_port,
             path=mission_url,
         )
+
+        self.logger.debug('Proxying to {url}'.format(url=url))
 
         try:
             resp = request_func(
