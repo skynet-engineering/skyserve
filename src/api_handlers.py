428d944bcbc407582ba3f38a29ed8fb301ec8e03
Kevin Lin <developer@kevinlin.info>
2017-11-13 10:48:21 -0600

    Deprecate avionics logic
    Summary: This is no longer a responsibility of Skyserve since it is taken care of by Skyengine.
    
    Reviewers: andrew, #skynet
    
    Reviewed By: andrew, #skynet
    
    Maniphest Tasks: T115
    
    Differential Revision: https://code.skynet.engineering/D83

--- src/api_handlers.py
+++ src/api_handlers.py
@@ -3,7 +3,6 @@ from handlers.discover_handler import DiscoverHandler
 from handlers.example_async_handler import ExampleAsyncHandler
 from handlers.example_sync_handler import ExampleSyncHandler
 from handlers.mission_proxy_handler import MissionProxyHandler
-from handlers.takeoff_demo_handler import TakeoffDemoHandler
 
 
 # Every time a new handler is added, add its handler class to the list here.
@@ -13,5 +12,4 @@ HANDLERS = [
     ExampleAsyncHandler,
     ExampleSyncHandler,
     MissionProxyHandler,
-    TakeoffDemoHandler,
 ]
