1cad24932a9fc7903a02e510fcf8690966dfa97e
Kevin Lin <developer@kevinlin.info>
2017-12-04 09:02:29 -0600

    Verify Redis availability during context initialization
    Summary: Check Redis health using the `PING` command during server initialization. Will immediately error if unavailable, rather than giving the user a cryptic error on the first attempt to use it.
    
    Test Plan:
    What it looks like now if you don't have Redis installed:
    
    ```
    $ make serve
    PYTHONPATH=src python src/skyserve.py
    Starting copter simulator (SITL)
    SITL already Downloaded and Extracted.
    Ready to boot.
    >>> APM:Copter V3.3 (d6053245)
    >>> Frame: QUAD
    >>> Calibrating barometer
    >>> Initialising APM...
    >>> barometer calibration complete
    >>> GROUND START
    [08:59:39] [context] [ERROR] Redis server not available!
    Traceback (most recent call last):
      File "src/skyserve.py", line 13, in <module>
        ctx = Context()
      File "/home/kiwi/sync/rice-university/elec-494--senior-capstone-design/code/skyserve/src/context.py", line 34, in __init__
        self._verify_ctx()
      File "/home/kiwi/sync/rice-university/elec-494--senior-capstone-design/code/skyserve/src/context.py", line 37, in _verify_ctx
        self._verify_redis()
      File "/home/kiwi/sync/rice-university/elec-494--senior-capstone-design/code/skyserve/src/context.py", line 48, in _verify_redis
        'See installation instructions here: https://redis.io/download',
    exc.SkyserveException: Unable to connect to Redis server at port 6379! Are you sure it is installed and running? Redis is required to self-register a mission with Skyserve. See installation instructions here: https://redis.io/download
    Makefile:11: recipe for target 'serve' failed
    make: *** [serve] Error 1
    ```
    
    Reviewers: rjcunningham, #skynet
    
    Reviewed By: rjcunningham
    
    Maniphest Tasks: T175
    
    Differential Revision: https://code.skynet.engineering/D142

--- src/context.py
+++ src/context.py
@@ -1,9 +1,11 @@
 import os
 
+from redis import ConnectionError
 from redis import StrictRedis
 from skyengine.drone import DroneController
 from skysense.camera import ImageClient
 
+from exc import SkyserveException
 from logger import Logger
 
 logger = Logger(__name__)
@@ -27,3 +29,29 @@ class Context(object):
 
         # Querying registered mission services
         self.redis = StrictRedis()
+
+        # Verify the instantiated context before continuing
+        self._verify_ctx()
+
+    def _verify_ctx(self):
+        """
+        Verify health of all necessary clients instantiated by the context.
+        """
+        self._verify_redis()
+
+    def _verify_redis(self):
+        """
+        Verify health and availability of the Redis server.
+
+        :raises SkyserveException: If a Redis server is not available on the default port.
+        """
+        try:
+            self.redis.ping()
+        except ConnectionError:
+            logger.error('Redis server not available!')
+            raise SkyserveException(' '.join([
+                'Unable to connect to Redis server at port 6379!',
+                'Are you sure it is installed and running?',
+                'Redis is required to self-register a mission with Skyserve.',
+                'See installation instructions here: https://redis.io/download',
+            ]))
---
+++ src/exc.py
@@ -0,0 +1,5 @@
+class SkyserveException(Exception):
+    """
+    Exception raised for any configuration errors detected by Skyserve application logic.
+    """
+    pass
